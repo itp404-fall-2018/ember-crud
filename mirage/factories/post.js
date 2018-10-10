2148d3341b8c9d050fac8227ad006fb1c22f4500
David Tang <dtang859@gmail.com>
2018-10-09 18:15:24 -0700

    revert changes

--- mirage/factories/post.js
+++ mirage/factories/post.js
@@ -1,25 +1,13 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
+  id(i) {
+    return i + 1;
+  },
   title(i) {
     return `Post ${i + 1}`;
   },
   body() {
     return faker.lorem.sentence();
-  },
-  price() {
-    return faker.commerce.price();
-  },
-  productName() {
-    return faker.commerce.productName();
-  },
-  color() {
-    return faker.commerce.color();
-  },
-  productMaterial() {
-    return faker.commerce.productMaterial();
-  },
-  id() {
-    return faker.helpers.slugify(this.productName);
   }
 });
